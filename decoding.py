e80424c9ecb3e87116876ab6a3c7061bd8417abf
MichaelBeehler <michael.beehler@spartans.ut.edu>
2025-12-02 21:47:12 -0500

    Added Sample Code from MNE documentation

---
+++ decoding.py
@@ -0,0 +1,83 @@
+#############################################################################
+# Note: This code was adopted from: https://mne.tools/stable/auto_examples/decoding/decoding_csp_eeg.html
+# Authors: Martin Billinger <martin.billinger@tugraz.at>
+# License: BSD-3-Clause
+# Copyright the MNE-Python contributors.
+#############################################################################
+
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.model_selection import ShuffleSplit, cross_val_score
+from sklearn.pipeline import Pipeline
+
+from mne import Epochs, pick_types
+from mne.channels import make_standard_montage
+from mne.datasets import eegbci
+from mne.decoding import CSP, get_spatial_filter_from_estimator
+from mne.io import concatenate_raws, read_raw_edf
+
+print(__doc__)
+
+# #############################################################################
+# # Set parameters and read data
+
+# avoid classification of evoked responses by using epochs that start 1s after
+# cue onset.
+tmin, tmax = -1.0, 4.0
+subjects = 1
+runs = [6, 10, 14]  # motor imagery: hands vs feet
+
+raw_fnames = eegbci.load_data(subjects, runs)
+raw = concatenate_raws([read_raw_edf(f, preload=True) for f in raw_fnames])
+eegbci.standardize(raw)  # set channel names
+montage = make_standard_montage("standard_1005")
+raw.set_montage(montage)
+raw.annotations.rename(dict(T1="hands", T2="feet"))  # as documented on PhysioNet
+raw.set_eeg_reference(projection=True)
+
+# Apply band-pass filter
+raw.filter(7.0, 30.0, fir_design="firwin", skip_by_annotation="edge")
+
+picks = pick_types(raw.info, meg=False, eeg=True, stim=False, eog=False, exclude="bads")
+
+# Read epochs (train will be done only between 1 and 2s)
+# Testing will be done with a running classifier
+epochs = Epochs(
+    raw,
+    event_id=["hands", "feet"],
+    tmin=tmin,
+    tmax=tmax,
+    proj=True,
+    picks=picks,
+    baseline=None,
+    preload=True,
+)
+epochs_train = epochs.copy().crop(tmin=1.0, tmax=2.0)
+labels = epochs.events[:, -1] - 2
+
+# Define a monte-carlo cross-validation generator (reduce variance):
+scores = []
+epochs_data = epochs.get_data(copy=False)
+epochs_data_train = epochs_train.get_data(copy=False)
+cv = ShuffleSplit(10, test_size=0.2, random_state=42)
+cv_split = cv.split(epochs_data_train)
+
+# Assemble a classifier
+lda = LinearDiscriminantAnalysis()
+csp = CSP(n_components=4, reg=None, log=True, norm_trace=False)
+
+# Use scikit-learn Pipeline with cross_val_score function
+clf = Pipeline([("CSP", csp), ("LDA", lda)])
+scores = cross_val_score(clf, epochs_data_train, labels, cv=cv, n_jobs=None)
+
+# Printing the results
+class_balance = np.mean(labels == labels[0])
+class_balance = max(class_balance, 1.0 - class_balance)
+print(f"Classification accuracy: {np.mean(scores)} / Chance level: {class_balance}")
+
+# plot eigenvalues and patterns estimated on full data for visualization
+csp.fit_transform(epochs_data, labels)
+spf = get_spatial_filter_from_estimator(csp, info=epochs.info)
+spf.plot_scree()
+spf.plot_patterns(components=np.arange(4))
